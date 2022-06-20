acb296ebaedda63dc00d8370c2c4504fb45b468a
huanblog <60742382+huanblog@users.noreply.github.com>
2022-06-20 14:07:40 +0800

    Update select_class.js

--- select_class.js
+++ select_class.js
@@ -1,12 +1,15 @@
 var i=1
 window.alert=function(){}
 var select = setInterval(function() {
-    electCourseTable.tip.submit({lessonId: 458831,lessonCount: 0,ele:this,type:jQuery(this).attr('operator'),virtualLoc:'list',virtualCashEnabled:false});
-    // if(($(".result-content").text() == "选课成功") || ($(".result-content").text() == "相同教学班只能选一次")) {
-    //     window.clearInterval(select);
-    //     console.log("选课成功！");
-    // } else {
+	electCourseTable.tip.submit({lessonId: 494846,lessonCount: 0,ele:this,type:jQuery(this).attr('operator'),virtualLoc:'list',virtualCashEnabled:false,type:'ELECTION'});
+	 if($("#cboxLoadedContent").text().indexOf('成功') > 0) {
+		window.clearInterval(select);
+    	console.log("选课成功！");
+     } else {
         console.log("第 " + i + " 次选课尝试");
         i++;
-    // }
-},200);
+     }
+},2000);
+var alert=function(){return 1}
+var confirm=function(){return 1}
+var prompt=function(){return 1}
